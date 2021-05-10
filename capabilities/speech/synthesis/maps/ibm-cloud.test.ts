c613a23c5268f574eb909511cab5b1fefd032a12
Jan Halama <jan.halama@superface.ai>
2021-05-10 13:06:16 +0200

    feat(speech): add IBM cloud provider (speech synthesis use-case)

---
+++ capabilities/speech/synthesis/maps/ibm-cloud.test.ts
@@ -0,0 +1,73 @@
+import fs from 'fs';
+import { SuperfaceClient } from '../../../../superface/sdk';
+
+describe('speech/synthesis/ibm-cloud-typed', () => {
+  it('should define use-case and provider', async () => {
+    const client = new SuperfaceClient();
+    const profile = await client.getProfile('speech/synthesis');
+    const provider = await client.getProvider('ibm-cloud');
+    const usecase = profile.useCases.TextToSpeechSynthesis;
+
+    expect(provider).not.toBeUndefined();
+    expect(usecase).not.toBeUndefined();
+  });
+
+  it.only('should synthetise text to speech ecoded linear_pcm', async () => {
+    const client = new SuperfaceClient();
+    const profile = await client.getProfile('speech/synthesis');
+    const provider = await client.getProvider('ibm-cloud');
+
+    // eslint-disable-next-line
+    const result = await profile.useCases.TextToSpeechSynthesis.perform(
+      {
+        text: 'Hello',
+        voice: { languageCode: 'en' },
+        audio: { encoding: 'linear_pcm' },
+      },
+      {
+        provider: provider,
+      }
+    );
+    console.debug(result);
+    // eslint-disable-next-line
+    expect(result.isOk()).toBeTruthy();
+    // eslint-disable-next-line
+    const value = result.unwrap();
+    expect(value.audioContent).toBeDefined();
+    console.debug(value);
+    expect(
+      Buffer.from(value.audioContent ?? '', 'base64')
+        .toString('ascii')
+        .substring(0, 4)
+    ).toBe('RIFF');
+    fs.writeFileSync(
+      './test-ibm.wav',
+      Buffer.from(value.audioContent ?? '', 'base64'),
+      'binary'
+    );
+  });
+
+  it('should synthetise text to speech encoded mp3', async () => {
+    const client = new SuperfaceClient();
+    const profile = await client.getProfile('speech/synthesis');
+    const provider = await client.getProvider('ibm-cloud');
+
+    // eslint-disable-next-line
+    const result = await profile.useCases.TextToSpeechSynthesis.perform(
+      {
+        text: 'Hello',
+        voice: { languageCode: 'en' },
+        audio: { encoding: 'mp3' },
+      },
+      {
+        provider: provider,
+      }
+    );
+    // eslint-disable-next-line
+    expect(result.isOk()).toBeTruthy();
+    // eslint-disable-next-line
+    const value = result.unwrap();
+    expect(value.audioContent).toBeDefined();
+    expect(Buffer.from(value.audioContent ?? '', 'base64')).toBeDefined();
+  });
+});
